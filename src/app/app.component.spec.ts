9e989a0f06fd4b131a1c67487cb56e70b1593d38
Max Houston Ramirez Martel <ogti_temp126@produce.gob.pe>
2022-12-07 23:51:58 -0500

    two commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'actividad12'`, () => {
+  it(`should have as title 'actividad-12'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('actividad12');
+    expect(app.title).toEqual('actividad-12');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('actividad12 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('actividad-12 app is running!');
   });
 });
